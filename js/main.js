14924b0f7506f9bfaa8328028d8f1985ccf1458c
Dev P. Sharma <44609609+codewithdev@users.noreply.github.com>
2019-10-11 12:17:15 +0530

    Create main.js
    Added the main.js in the js folder for the "javascripts.html" in reference with "Scroll to Up"

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*======For Scroll to Top==============*/
+
+
+
+$('.c-top').click(function () {
+    $('html, body').stop().animate({ scrollTop: 0 }, 'slow', 'swing');
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > $(window).height()) {
+      $('.c-top').addClass("c-top--active");
+    } else {
+      $('.c-top').removeClass("c-top--active");
+    };
+  });
+
+
+});
