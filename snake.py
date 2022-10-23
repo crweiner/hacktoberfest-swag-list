aea42c899527f653d3144debb739d0af82a78607
Senuth Dilnada <71760760+Dila-wa@users.noreply.github.com>
2022-10-23 19:29:48 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -15,7 +15,7 @@ dis_width = 600
 dis_height = 400
  
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Edureka')
+pygame.display.set_caption('Snake Game by Dila_Wa')
  
 clock = pygame.time.Clock()
  
