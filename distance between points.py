2cda2f408d6da42e800ab229763eccfd8799e4dc
Thambapanni-Designs <116666202+Thambapanni-Designs@users.noreply.github.com>
2024-10-17 08:02:57 +0530

    Create code to find distance between points
    Create code to find distance between points

---
+++ distance between points.py
@@ -0,0 +1,11 @@
+import math
+
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
+print("The distance between the points is :",distance)
